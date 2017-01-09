68277a60cb1335b69feeb575aed040abf17481a9
user <user@pavel>
2017-01-09 17:51:17 +0300

    Initial commit

---
+++ config/common/console.php
@@ -0,0 +1,38 @@
+<?php
+
+$params = require(__DIR__ . '../'.YII_ENV.'/params.php');
+$commonParams = require (__DIR__ . '/params.php');
+$params = array_merge($commonParams, $params);
+$db = require(__DIR__ . '../'.YII_ENV.'/db.php');
+
+$config = [
+    'id' => 'basic-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
